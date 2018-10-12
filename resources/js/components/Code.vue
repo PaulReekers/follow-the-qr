5a753f82a0a66d6de1278044a64ac9ea22218035
Paul <paulreekers@gmail.com>
2018-10-12 12:53:44 +0200

    styling something

--- resources/js/components/Code.vue
+++ resources/js/components/Code.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="code">
+<<<<<<< Updated upstream
       <div class="page-header header-filter" style="background-image: url('/img/bg.jpg'); background-size: cover; background-position: top center;">
         <div class="container">
           <div class="row">
@@ -36,6 +37,45 @@
           </div>
         </div>
       </div>
+=======
+        <div class="page-header header-filter" style="background-image: url('/img/bg.jpg'); background-size: cover; background-position: top center;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+                        <div class="card">
+                            <form class="form" method="" action="">
+                                <div class="card-header card-header-primary text-center">
+                                    <h4 class="card-title">Hey hello, <br>lets add your current location to the journey!</h4>
+                                </div>
+                                <p class="description text-center">51.4442192,5.4717674</p>
+                                <div class="card-body">
+                                    <div class="input-group">
+                                        <ui-textbox
+                                            enforce-maxlength
+                                            help="Maximum 140 characters"
+                                            label="Leave a message for others to see"
+                                            placeholder=""
+                                            icon="short_text"
+
+                                            :multi-line="true"
+                                            :maxlength="140"
+
+                                            v-model="gpsDescription"
+                                        ></ui-textbox>
+                                    </div>
+                                </div>
+                                <div class="footer text-center">
+                                    <a href="" class="btn btn-primary btn-link btn-wd btn-lg">Submit</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+
+>>>>>>> Stashed changes
     </div>
 </template>
 
@@ -55,5 +95,4 @@
 </script>
 
 <style>
-
 </style>
