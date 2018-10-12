d42bba44eba8c309a2f11f92c3d0e9810d1a9046
Paul <paulreekers@gmail.com>
2018-10-12 12:11:52 +0200

    template code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
