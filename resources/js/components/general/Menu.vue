385670b50917dcafd3003b7b6703f49d9ff9cbcf
Stan Ansems <stan.ansems@coosto.com>
2018-10-12 10:04:39 +0200

    Add menu

--- resources/js/components/general/Menu.vue
+++ resources/js/components/general/Menu.vue
@@ -1,6 +1,29 @@
 <template>
     <div id="menu">
-      <ui-toolbar></ui-toolbar>
+      <ui-toolbar>
+        <div slot="icon">
+          <ui-icon-button
+            color="black"
+            has-dropdown
+            type="default"
+            icon="menu"
+            ref="menu"
+            size="large"
+          >
+            <ui-menu
+              contain-focus
+              has-icons
+              slot="dropdown"
+              :options="menuOptions"
+              @close="$refs.menu.closeDropdown()"
+              @select="selectMenuitem"
+            ></ui-menu>
+          </ui-icon-button>
+        </div>
+        <div slot="default">
+          Follow the QR
+        </div>
+      </ui-toolbar>
     </div>
 </template>
 
@@ -8,21 +31,21 @@
   import { UiMenu, UiIconButton, UiToolbar } from 'keen-ui';
 
   export default {
+    data() {
+      return {
+        menuOptions: [
+          {label: 'Home', id: 'home', icon: 'place'},
+          {label: 'Why', id: 'why', icon: 'why'}
+        ]
+      }
+    },
     components: {
-      UiToolbar
+      UiToolbar,
+      UiMenu,
+      UiIconButton
     }
   }
 </script>
 
 <style>
-  #menu {
-    background-color: #DDD;
-  }
-
-  #logo {
-    float: left;
-  }
-
-  #slogan {
-  }
 </style>
