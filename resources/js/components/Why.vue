95a95274552cc231ab66bdc0b5eb155beeb3f7cb
Paul <paulreekers@gmail.com>
2018-10-12 14:34:06 +0200

    why

--- resources/js/components/Why.vue
+++ resources/js/components/Why.vue
@@ -1,6 +1,23 @@
 <template>
     <div id="why">
-      <h1>Why did we start follow the QR</h1>
+      <div class="page-header header-filter">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+              <div class="card card-login">
+                  <div class="card-header card-header-primary text-center">
+                    <h4 class="card-title">How it works</h4>
+                  </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
